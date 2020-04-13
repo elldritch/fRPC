6b847d09a9d2311acdac3427b70dfd456a093044
Leo Zhang <leo@leozhang.me>
2020-04-12 18:45:32 -0700

    feat: Add recipe and technology to mod

--- mod/data.lua
+++ mod/data.lua
@@ -13,16 +13,52 @@ data:extend({
   {
     type = "item",
     name = "frpc-sensor",
-    icon = "__base__/graphics/icons/constant-combinator.png",
+    icon = "__base__/graphics/icons/small-lamp.png",
     icon_size = 32,
     place_result = "frpc-sensor",
     subgroup = "circuit-network",
     order = "s",
-    stack_size = 10,
-  }
+    stack_size = 50,
+  },
 
-  -- TODO: Recipe.
+  -- Recipe.
+  {
+    type = "recipe",
+    name = "frpc-sensor",
+    ingredients =
+    {
+      {"copper-cable", 5},
+      {"electronic-circuit", 5},
+    },
+    result = "frpc-sensor",
+    enabled = false
+  },
 
-
-  -- TODO: Technology.
+  -- Technology.
+  -- TODO: add custom icon.
+  {
+    type = "technology",
+    name = "remote-control",
+    icon_size = 128,
+    icon = "__base__/graphics/technology/circuit-network.png",
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "frpc-sensor"
+      }
+    },
+    prerequisites = {"circuit-network", "automation-2"},
+    unit =
+    {
+      count = 150,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1}
+      },
+      time = 15
+    },
+    order = "r-s-f"
+  },
 })
